d6380de669dcfff85cad4b5abaa22a5739b3b1e3
Raf C <130236858+raf-c@users.noreply.github.com>
2023-10-18 23:26:55 +0200

    Add basic operator functions

---
+++ script.js
@@ -0,0 +1,27 @@
+function add (a,b){
+    return a + b;
+}
+
+function subtract (a,b){
+    return a - b;    
+}
+
+function multiply (a,b){
+    return a * b;  
+}
+
+function divide (a,b){
+    return a / b;  
+}
+
+function operate (a,b,operator) {
+    if (operator=="+"){
+        return add(a,b);
+    } else if (operator=="-") {
+        return subtract(a.b);
+    } else if (operator=="x") {
+        return multiply(a,b);
+    } else if (operator=="/") {
+        return divide(a,b);
+    }
+}
